117d4048a80f92fe64ff0b6ddee5f922966686c8
Yuch Sun <yuchins@yahoo.com>
2024-01-09 16:34:04 +0000

    commit changes

--- models/Training/store_2023.sql
+++ models/Training/store_2023.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='table' )}}
-With store_tbl_2023 as (
+With store_2023 as (
     Select S_STORE_SK, S_STORE_ID, S_STORE_NAME, S_COUNTY From {{ source('test_prod', 'Store_2023')}}
 )
-Select * From store_tbl_2023
+Select * From store_2023
