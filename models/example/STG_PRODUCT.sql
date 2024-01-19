7856a747111d2c63ccadd501087106fabff5a731
Yuch Sun <yuchins@yahoo.com>
2024-01-19 19:52:56 +0000

    sync changes

--- models/example/STG_PRODUCT.sql
+++ models/example/STG_PRODUCT.sql
@@ -1,7 +1,7 @@
 With Product as (
 
-select A.PRODUCT_ID, A.Product_Name, A.state, A.Region, B.Unit_SOLD, B.Amount
-from Prod.Dim_Product A inner join Prod.Fact_Sales B
+select A.PRODUCT_ID, A.Product_Name, A.PRODUCT_CATEGORY, A.state, A.Region, A.Brand, B.quantity,  B.Amount
+from Prod.Dim_Product A inner join Prod.FACT_PRODUCT_SALES B
 on A.PRODUCT_ID=B.PRODUCT_ID
 
 )
--- tests/unit_sold_greater_than_Zero.sql
+++ tests/unit_sold_greater_than_Zero.sql
@@ -1,2 +1,2 @@
 select * from {{ref('STG_PRODUCT')}}
-where Unit_SOLD < 0
+where quantity < 0
